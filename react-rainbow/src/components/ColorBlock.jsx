a29a2bc0aebdd00ba34646008512c7f8d4cabbea
unknown <jasmina_t05@outlook.com>
2023-04-11 21:37:58 -0700

    completed activity

---
+++ react-rainbow/src/components/ColorBlock.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function ColorBlock({color}){
+
+    return <div className="ColorBlock"
+    style={{'backgroundColor':color}}
+    >
+        <p>{color}</p>
+    </div>
+
+
+}
